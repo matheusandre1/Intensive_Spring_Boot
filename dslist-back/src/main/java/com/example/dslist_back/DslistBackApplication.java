4a788181c332ae99f5a3c35d37e6ab50b122ecf0
Matheus Andre <matheusandr2@gmail.com>
2025-05-21 23:30:10 -0300

    feat: create project spring boot

---
+++ dslist-back/src/main/java/com/example/dslist_back/DslistBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dslist_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistBackApplication.class, args);
+	}
+
+}
